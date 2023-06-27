d308ac1478bc92aeea06fd054d8ae739be9f97fb
TroyLynx <58670183+Andoh-Jeffery@users.noreply.github.com>
2023-06-27 22:12:34 +0100

    Added_log_in_screen

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express=require('express')
-const session=require('express-session')
-const FireStoreStore=require("firestore-store")(session)
-const bcrypt=require('bcrypt')
+const express = require('express')
+const session = require('express-session')
+const path=require('path')
+const FireStoreStore = require("firestore-store")(session)
+const bcrypt = require('bcrypt')
 require('dotenv').config()
-const db=require('./config/db')
-const student=require('./routes/student')
-const teacher=require('./routes/teacher')
+const db = require('./config/db')
+const student = require('./routes/student')
+const teacher = require('./routes/teacher')
 const firestoreStore = require('firestore-store')
-const app=express()
+const { log } = require('console')
+const app = express()
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}))
-app.use('/student',student)
-app.use('/teacher',teacher)
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname,"./public")))
+app.use('/student', student)
+app.use('/teacher', teacher)
 
 // SESSION
-const store=new FireStoreStore({
-    database:db,
-    collection:"session",
+const store = new FireStoreStore({
+    database: db,
+    collection: "session",
 })
 app.use(
     session({
-      secret: "the key for the secret",
-      resave: false,
-      saveUninitialized: false,
-      store: store
+        secret: "the key for the secret",
+        resave: false,
+        saveUninitialized: false,
+        store: store
     })
-  );
+);
 
 
 // MIDDLEWARE
-const isAuth=(req,res,next)=>{
-    if(req.session.isAuth){
+const isAuth = (req, res, next) => {
+    if (req.session.isAuth) {
         next()
     }
-    console.log("wrong cred");
+    // else{
+    //     // res.redirect('/login')
+    // }
 }
 
 // const isAuthorize=(req,res,next)=>{
@@ -47,33 +52,38 @@ const isAuth=(req,res,next)=>{
 // }
 
 // GET REQUEST TO /
-app.get('/',(req,res)=>{
-    res.send('homepage')
+app.get('/', (req, res) => {
+    res.render('login')
+    // res.send('homepage')
 })
 // 
 // GET REQUEST TO /DASHBOARD
-app.get('/dashboard',isAuth,isAuthorize,(req,res)=>{
-    
-    res.render('dashboard')
+app.get('/dashboard', isAuth, (req, res) => {
+    // res.render('dashboard')
+    if (req.session.isAuthorize == 1) {
+        res.json("1")
+    }
+    else { res.json("2") }
 })
 // LOGIN
 
-app.post('/login',async(req,res)=>{
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
     try {
-        const { email, password } = req.body
-        const teacher = await db.collection('teacher').where('email', '==', req.body.email).get()
-        
+        console.log(email,password);
+        const teacher = await db.collection('teacher').where('email', '==', email).get()
+
         if (!teacher.empty) {
-            console.log("yes"); 
+            console.log("yes");
             teacher.forEach(async (teacher) => {
-                const isMatch = await bcrypt.compare(req.body.password, teacher.data().password)
+                const isMatch = await bcrypt.compare(password, teacher.data().password)
                 if (isMatch) {
                     console.log("matched");
                     // console.log(teacher.data().house);
-                        // console.log(tea.data().email);
-                        req.session.isAuth=true
-                        req.session.isAuthorize=teacher.data().house
-                        res.render('dashboard')
+                    // console.log(tea.data().email);
+                    req.session.isAuth = true
+                    req.session.isAuthorize = teacher.data().house
+                    res.redirect('dashboard')
                 }
                 else {
                     console.log("no match");
@@ -89,4 +99,4 @@ app.post('/login',async(req,res)=>{
 })
 
 
-app.listen(process.env.PORT,(console.log(`listening on port http://localhost:${process.env.PORT}`)))
+app.listen(process.env.PORT, (console.log(`listening on port http://localhost:${process.env.PORT}`)))
